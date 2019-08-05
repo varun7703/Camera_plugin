733fcbc5b2e097e091ac8914e73e9f945f67b2c6
varun7703 <38864825+varun7703@users.noreply.github.com>
2019-08-05 22:08:13 +0530

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {Facebook, FacebookLoginResponse} from "@ionic-native/facebook";
+import { AndroidFullScreen } from '@ionic-native/android-full-screen';
+import { Shake } from '@ionic-native/shake';
+import { Platform } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+im:any;
+  constructor(private camera: Camera ) {
 
   }
 
+fullscreen(){
+  const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64 (DATA_URL):
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    this.im = base64Image;
+   }, (err) => {
+    // Handle error
+   });
+}
 }
